e020bf11c5779f708e938f2acadf4b3c69592e74
Jun Aruga <jaruga@redhat.com>
2020-03-23 15:40:14 +0100

    Set script/bench.rb as absolute path to run test.sh from any directory.

--- script/test.sh
+++ script/test.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+TOP_DIR="$(dirname "${0}")/.."
+
 function clear_tmp {
     rm -rf "${TMP_DIR}"
     mkdir "${TMP_DIR}"
@@ -44,17 +46,17 @@ ls "${TMP_DIR}" | grep -q '^_ruby_mjit_.*\.so$'
 # 2: Benchmark tests
 echo "=== Benchmark tests ==="
 
-ruby script/bench.rb
-ruby --jit script/bench.rb
+ruby "${TOP_DIR}/script/bench.rb"
+ruby --jit "${TOP_DIR}/script/bench.rb"
 # On Ruby 2.7, the default values of
 # --jit-min-calls is changed from 5 to 10000.
 # --jit-max-cache is changed from 1000 to 100.
 # This test is to do benchmark by Ruby 2.6's default values.
 # https://github.com/ruby/ruby/commit/0fa4a6a618295d42eb039c65f0609fdb71255355
-ruby --jit --jit-min-calls=5 --jit-max-cache=1000 script/bench.rb
+ruby --jit --jit-min-calls=5 --jit-max-cache=1000 "${TOP_DIR}/script/bench.rb"
 
 clear_tmp
-TMP="${TMP_DIR}" ruby --jit --jit-verbose=2 --jit-save-temps --jit-min-calls=5 --jit-max-cache=1000 script/bench.rb
+TMP="${TMP_DIR}" ruby --jit --jit-verbose=2 --jit-save-temps --jit-min-calls=5 --jit-max-cache=1000 "${TOP_DIR}/script/bench.rb"
 ls -1 "${TMP_DIR}"
 ls "${TMP_DIR}" | grep -q '^_ruby_mjit_.*\.gch$'
 ls "${TMP_DIR}" | grep -q '^_ruby_mjit_.*\.so$'
